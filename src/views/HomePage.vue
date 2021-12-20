dff7745c934628625aec43d3a94d51d359527928
Ebenezer Oladutemu <eoladutemu@parkwayprojects.com>
2021-12-21 00:07:43 +0100

    Update Homepage intro

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="home">
+    <h1>
+      Do you think you know about Venture Capital?<br>
+      Do you want to know more?<br>
+    </h1>
+    <h2>
+      Take this quick and simple quiz and test your knowledge on Venture Capital.<br>
+      Click on the submit button at the bottom of the next page to get your results. Goodluck!
+    </h2>
+    <router-link to="questions">Start Quiz</router-link>
   </div>
 </template>
 
@@ -9,3 +18,7 @@ export default {
   name: 'Home',
 }
 </script>
+
+<style scoped>
+
+</style>
