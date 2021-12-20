f5952e15b4293e4829d67ebbad1b9c6d9c69b2c8
Ebenezer Oladutemu <eoladutemu@parkwayprojects.com>
2021-12-21 00:06:12 +0100

    Create and show Results.

---
+++ src/views/Result.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="padding: 1rem">
+    
+    <div class="result" v-for="(result, i) in results" :key="result">
+      <p>{{ i + 1 }}. {{ result.split(' -')[0] }}</p>
+      <span>
+        Your answer - <strong>{{ result.split(' -')[1] }}. </strong>
+        <span>Correct!/Wrong</span>
+      </span>
+      <p>Correct Answer - </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      results: []
+    }
+  },
+  mounted() {
+    let i = 1
+    while (i <= 10) {
+      console.log(localStorage.getItem(parseInt(i)))
+      this.results.push(localStorage.getItem(parseInt(i)))
+      i++;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
