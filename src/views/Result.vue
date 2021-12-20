cb407f0b20f5f325cb8880b67afb42472714ee1e
Ebenezer Oladutemu <eoladutemu@parkwayprojects.com>
2021-12-21 00:06:55 +0100

    Update results styles

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,6 +1,10 @@
 <template>
   <div style="padding: 1rem">
-    
+    <div class="text-center">
+      <h1>Your Results are here!</h1>
+      <p class="score">70%</p>
+      <p style="font-size: 1.2rem">You 7 out of 10 questions</p>
+    </div>
     <div class="result" v-for="(result, i) in results" :key="result">
       <p>{{ i + 1 }}. {{ result.split(' -')[0] }}</p>
       <span>
@@ -31,5 +35,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.result {
+  p:first-child {
+    margin-bottom: 0.5rem;
+    font-size: 1.5rem;
+  }
+  span, p:last-child {
+    font-size: 1.1rem;
+    margin-top: 0.5rem;
+  }
+}
 
+.score {
+  padding: 2rem;
+  font-size: 2rem;
+  border: 1px solid #FBBB07;
+  width: fit-content;
+  border-radius: 50%;
+  margin: auto;
+}
 </style>
